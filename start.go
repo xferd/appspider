1baabce8234e713a54feec0ca3b3acea06a1612d
Li Yan <liyan34@lenovo.com>
2017-10-25 10:04:54 +0800

    sync waitgroup

--- start.go
+++ start.go
@@ -3,19 +3,32 @@ package main
 import (
     "github.com/xferd/appspider/spider"
     "log"
+    "sync"
+)
+
+var (
+    wg sync.WaitGroup
 )
 
 func main() {
     var ch_http = make(chan string, 256)
-    for pkgname := spider.NextPackage(); pkgname != ""; pkgname = spider.NextPackage() {
-        ch_http <- pkgname
-        go work(pkgname, ch_http)
+    for {
+        for pkgname := spider.NextPackage(); pkgname != ""; pkgname = spider.NextPackage() {
+            ch_http <- pkgname
+            wg.Add(1)
+            go work(pkgname, ch_http)
+        }
+        // wg.Wait()
     }
 }
 
 func work(pkgname string, ch chan string) {
+    var work_wg sync.WaitGroup
+
     defer func() {
         <- ch
+        work_wg.Wait()
+        wg.Done()
         }()
 
     rawurl := "https://play.google.com/store/apps/details?id=" + pkgname
@@ -27,12 +40,14 @@ func work(pkgname string, ch chan string) {
     }
 
     if _, ok := spider.SaveHTMLFile(pkgname, html); ok {
+        work_wg.Add(1)
         go func() {
             new_pkgnames := spider.FindPkgnames(&html)
             for _, new_pkg := range new_pkgnames {
                 spider.AddPackage(new_pkg)
             }
-            go spider.UpdatePackage(pkgname)
+            spider.UpdatePackage(pkgname)
+            work_wg.Done()
         }()
     }
 }
